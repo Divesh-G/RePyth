d679da5da12fe92d86b02c31eb03f0a0528317a8
Dibash Giri <dibashgiri999@gmail.com>
2026-05-11 10:57:29 +0545

    numbers of list

---
+++ Loops/loops.py
@@ -0,0 +1,19 @@
+# form 1 to 100
+count = 1
+while count <= 5 :
+    print("Hello")
+    count += 1
+
+print(count)
+
+# neagtive indexing
+i = 5
+while i >= 1:
+    print("Divesh", i)
+    i -= 1
+
+#Table using loop
+num = int(input("Enter a number: "))
+while i <= 10:
+    print(i * num)
+    i += 1
